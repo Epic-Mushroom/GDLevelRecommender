7123cf4be1d0e83b03421d4e96d4a7259d71bc39
Epic-Mushroom <malroop1@outlook.com>
2026-07-02 13:37:41 -0700

    add api error handling

--- src/main.js
+++ src/main.js
@@ -1,5 +1,40 @@
+import * as recs from "./recommendations.js";
+
 const GDDL_API_URL = "https://gdladder.com/api";
 const PROXY_URL = `https://corsproxy.io/?${encodeURIComponent(GDDL_API_URL)}`;
+
+class APIError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "APIError";
+    }
+}
+
+/**
+ * 
+ * @param {Array<string>} pathVariables 
+ * @param {Object} queryParams 
+ */
+async function getAPIResponseText(pathVariables, queryParams) {
+    let resultURL = PROXY_URL;
+
+    for (const variable of pathVariables) {
+        resultURL += `/${encodeURIComponent(variable)}`;
+    }
+
+    const query = new URLSearchParams(queryParams).toString();
+    if (query.length > 0) {
+        resultURL += `?${query}`;
+    }
+
+    const response = await fetch(resultURL);
+
+    if (!response.ok) {
+        throw new APIError(`${response.status}: ${(await response.json()).message}`);
+    }
+    
+    return await response.text();
+}
 
 /**
  * 
@@ -19,7 +54,12 @@ async function getAPIResponse(pathVariables, queryParams) {
     }
 
     const response = await fetch(resultURL);
-    return await response.text();
+
+    if (!response.ok) {
+        throw new APIError(`${response.status}: ${(await response.json()).message}`);
+    }
+
+    return await response.json();
 }
 
-console.log(getAPIResponse(["submissions", "pending"], {limit: 3}).then(data => console.log(data)));
+console.log(getAPIResponseText(["user", "me"], {}).then(data => console.log(data)));
